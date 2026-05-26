97a812f5d56007250417044902f690f4713596fd
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2026-05-26 18:52:41 -0500

    Add credit cards frontend API module

---
+++ frontend/src/modules/credit-cards/creditCardsApi.ts
@@ -0,0 +1,13 @@
+import api from '@/lib/api'
+
+export const creditCardsAPI = {
+  summary: () => api.get('/credit-cards/summary'),
+  cards: () => api.get('/credit-cards/cards'),
+  createCard: (data: any) => api.post('/credit-cards/cards', data),
+  updateCard: (id: number, data: any) => api.put(`/credit-cards/cards/${id}`, data),
+  charges: () => api.get('/credit-cards/charges'),
+  createCharge: (data: any) => api.post('/credit-cards/charges', data),
+  installments: () => api.get('/credit-cards/installments'),
+  payments: () => api.get('/credit-cards/payments'),
+  createPayment: (data: any) => api.post('/credit-cards/payments', data),
+}
